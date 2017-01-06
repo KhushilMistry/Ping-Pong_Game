9e391a98a91ebc194b15f20b9324e8355111e4d3
Khushil Mistry <khushil324@gmail.com>
2017-01-07 00:18:30 +0530

    Movement of ball

--- ping_pong.js
+++ ping_pong.js
@@ -6,6 +6,12 @@ function Game() {
     this.context.fillStyle = "white";
     this.keys = new KeyListener();
 
+    this.ball = new Ball();
+    this.ball.x = this.width / 2;
+    this.ball.y = this.height / 2;
+    this.ball.vy = Math.floor(Math.random() * 12 - 6);
+    this.ball.vx = 7 - Math.abs(this.ball.vy);
+
     this.p1 = new Paddle(5, 0);
     this.p1.y = this.height / 2 - this.p1.height / 2;
     this.p2 = new Paddle(this.width - 5 - 2, 0);
@@ -16,6 +22,8 @@ Game.prototype.draw = function () {
     this.context.clearRect(0, 0, this.width, this.height);
     this.context.fillRect(this.width / 2, 0, 2, this.height);
 
+    this.ball.draw(this.context);
+
     this.p1.draw(this.context);
     this.p2.draw(this.context);
 };
@@ -23,6 +31,13 @@ Game.prototype.draw = function () {
 Game.prototype.update = function () {
     if (this.paused)
         return;
+
+    this.ball.update();
+    if (this.ball.x > this.width || this.ball.x + this.ball.width < 0) {
+        this.ball.vx = -this.ball.vx;
+    } else if (this.ball.y > this.height || this.ball.y + this.ball.height < 0) {
+        this.ball.vy = -this.ball.vy;
+    }
 
     if (this.keys.isPressed(83)) {
         this.p1.y = Math.min(this.height - this.p1.height, this.p1.y + 4);
@@ -35,6 +50,57 @@ Game.prototype.update = function () {
     } else if (this.keys.isPressed(38)) {
         this.p2.y = Math.max(0, this.p2.y - 4);
     }
+    
+    if (this.ball.vx > 0) {
+        if (this.p2.x <= this.ball.x + this.ball.width &&
+            this.p2.x > this.ball.x - this.ball.vx + this.ball.width) {
+            var collisionDiff = this.ball.x + this.ball.width - this.p2.x;
+            var k = collisionDiff / this.ball.vx;
+            var y = this.ball.vy * k + (this.ball.y - this.ball.vy);
+            if (y >= this.p2.y && y + this.ball.height <= this.p2.y + this.p2.height) {
+                // collides with right paddle
+                this.ball.x = this.p2.x - this.ball.width;
+                this.ball.y = Math.floor(this.ball.y - this.ball.vy + this.ball.vy * k);
+                this.ball.vx = -this.ball.vx;
+            }
+        }
+    } else {
+        if (this.p1.x + this.p1.width >= this.ball.x) {
+            var collisionDiff = this.p1.x + this.p1.width - this.ball.x;
+            var k = collisionDiff / -this.ball.vx;
+            var y = this.ball.vy * k + (this.ball.y - this.ball.vy);
+            if (y >= this.p1.y && y + this.ball.height <= this.p1.y + this.p1.height) {
+                // collides with the left paddle
+                this.ball.x = this.p1.x + this.p1.width;
+                this.ball.y = Math.floor(this.ball.y - this.ball.vy + this.ball.vy * k);
+                this.ball.vx = -this.ball.vx;
+            }
+        }
+    }
+
+    // Top and bottom collision
+    if ((this.ball.vy < 0 && this.ball.y < 0) ||
+        (this.ball.vy > 0 && this.ball.y + this.ball.height > this.height)) {
+        this.ball.vy = -this.ball.vy;
+    }
+};
+
+function Ball() {
+    this.x = 0;
+    this.y = 0;
+    this.vx = 0;
+    this.vy = 0;
+    this.width = 4;
+    this.height = 4;
+}
+
+Ball.prototype.update = function () {
+    this.x += this.vx;
+    this.y += this.vy;
+};
+
+Ball.prototype.draw = function (p) {
+    p.fillRect(this.x, this.y, this.width, this.height);
 };
 
 function KeyListener() {
